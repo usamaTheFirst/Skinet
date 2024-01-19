d9e594791a5ad213f72fa9a70e2c7553090279fe
usamaTheFirst <usamafiaz1453@gmail.com>
2024-01-19 18:29:27 +0500

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+
+    {
+        private readonly StoreContext context;
+
+        public ProductsController(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            return Ok(product);
+        }
+
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext :DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData( new Product()
+            {
+                Id=1,
+                Name="Product 1"
+            }, new Product()
+            {
+                Id = 2,
+                Name = "Product 2"
+            },new Product()
+            {
+                Id = 3,
+                Name = "Product 3"
+            },new Product()
+            {
+                Id = 4,
+                Name = "Product 3"
+            });
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
